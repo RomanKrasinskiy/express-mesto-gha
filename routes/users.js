1474076c369fd6980e0f676c5d5af26607254700
RomanKrasinskiy <krasinskii_roma@mail.ru>
2023-04-28 14:19:30 +0300

    registration and authorization on the backend. stage 1.1

--- routes/users.js
+++ routes/users.js
@@ -27,7 +27,7 @@ router.patch('/me', celebrate({
 
 router.patch('/me/avatar', celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string().regex(regAvatarURL).required(),
+    avatar: Joi.string().regex(RegExp(regAvatarURL)).required(),
   }),
 }), updateAvatar);
 
