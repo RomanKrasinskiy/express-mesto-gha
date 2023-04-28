e411ede7de6f9026e936c36c35d7834c97bd5fd1
RomanKrasinskiy <krasinskii_roma@mail.ru>
2023-04-28 19:13:54 +0300

    registration and authorization on the backend. stage 1.1

--- models/user.js
+++ models/user.js
@@ -22,7 +22,7 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     required: false,
-    validate: (value) => regAvatarURL.test(value),
+    validate: (value) => /https?:\/\/w{0,3}?[a-z0-9-]{1,}\..+#?/i.test(value),
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
   email: {
